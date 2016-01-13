78af122028d609484cd7d80ec84a005a188098f5
Robert M. Ryan <robert.ryan@mindspring.com>
2016-01-13 11:51:01 -0800

    Initial Commit

---
+++ NSURLSessionMultipart/NSURLSessionMultipart.h
@@ -0,0 +1,19 @@
+//
+//  NSURLSessionMultipart.h
+//  NSURLSessionMultipart
+//
+//  Created by Robert Ryan on 1/13/16.
+//  Copyright © 2016 Robert Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NSURLSessionMultipart.
+FOUNDATION_EXPORT double NSURLSessionMultipartVersionNumber;
+
+//! Project version string for NSURLSessionMultipart.
+FOUNDATION_EXPORT const unsigned char NSURLSessionMultipartVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSURLSessionMultipart/PublicHeader.h>
+
+
